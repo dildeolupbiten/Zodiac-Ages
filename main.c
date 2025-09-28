86c4c12c76fdc3122fa72599bc1da60e1f144856
dildeolupbiten <tckutlu@gmail.com>
2025-09-28 15:41:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,28 @@ static const char *SIGNS[12] = {
     "Lib", "Sco", "Sag", "Cap", "Aqu", "Pis"
 };
 
+typedef struct constellation_t {
+    char *name;
+    double start;
+    double end;
+} constellation_t;
+
+static constellation_t J2000[13] = {
+    {"Ari", 28.687, 53.417},
+    {"Tau", 53.417, 90.140},
+    {"Gem", 90.140, 117.988},
+    {"Can", 117.988, 138.038},
+    {"Leo", 138.038, 173.851},
+    {"Vir", 173.851, 217.810},
+    {"Lib", 217.810, 241.047},
+    {"Sco", 241.047, 247.638},
+    {"Oph", 247.638, 266.038},
+    {"Sag", 266.038, 299.656},
+    {"Cap", 299.656, 327.488},
+    {"Aqu", 327.488, 351.650},
+    {"Pis", 351.650, 28.687}
+};
+
 typedef struct date_t {
     int year;
     int month;
@@ -12,8 +34,8 @@ typedef struct date_t {
     double hour;
 } date_t;
 
-double julday(date_t date) {
-    return swe_julday(date.year, date.month, date.day, date.hour, SE_GREG_CAL);
+double julday(date_t dt) {
+    return swe_julday(dt.year, dt.month, dt.day, dt.hour, SE_GREG_CAL);
 }
 
 double calc_ut(
@@ -21,7 +43,8 @@ double calc_ut(
     int *d, 
     int *h, 
     int *m, 
-    int *index
+    int *index,
+    int constellation
 ) {
     double x2[6];
     char serr[256];
@@ -36,15 +59,34 @@ double calc_ut(
     if (ra < 0) {
         ra += 360;
     }
-    *index = ((int)ra / 30) % 12;
-    double degree = ra - 30 * *index;
+    double degree;
+    if (!constellation) {
+        *index = ((int)ra / 30) % 12;
+        degree = ra - 30 * *index;
+    } else {
+        for (int i = 0; i < 13; i++) {
+            degree = fmod(ra - J2000[i].start + 360, 360);
+            if (
+                degree >= 0 && 
+                degree < fmod(J2000[i].end - J2000[i].start + 360, 360.0)
+            ) {
+                *index = i;
+                break;
+            }
+        }
+    }
     *d = (int)degree;
     *h = (int)((degree - *d) * 60);
     *m = (int)((degree - *d - *h / 60.0) * 3600);
     return ra;
 }
 
-void find_zodiac_ages(date_t start, date_t end) {
+void find_zodiac_ages(
+    date_t start, 
+    date_t end, 
+    const int constellation, 
+    const int offset
+) {
     FILE *file = fopen("output.csv", "w");
     if (!file) {
         perror("Can't open file");
@@ -77,14 +119,15 @@ void find_zodiac_ages(date_t start, date_t end) {
                         &degree, 
                         &hour, 
                         &minute,
-                        &index
+                        &index,
+                        constellation
                     );
                     if (degree == 0 && hour == 0 && minute == 0) {
                         fprintf(
                             file,
                             "\"%d/03/21\",\"0\u00b0\",\"%s\"\n",
                             iyear,
-                            SIGNS[index]
+                            (constellation ? J2000[index].name : SIGNS[index])
                         );
                         fflush(file);
                         done = 1;
@@ -102,11 +145,13 @@ void find_zodiac_ages(date_t start, date_t end) {
     fclose(file);
 }
 
-int main() {
+int main(int argc, const char *argv[]) {
+    int constellation = argc == 1 ? 0 : atoi(argv[1]);
+    int offset = argc == 1 ? 1500 : atoi(argv[2]);
     date_t start = {-13000, 1, 1, 0};
     date_t end = {14000, 1, 1, 0};
     swe_set_ephe_path("lib/swe/eph");
     swe_set_sid_mode(SE_SIDM_FAGAN_BRADLEY, 0, 0);
-    find_zodiac_ages(start, end);
+    find_zodiac_ages(start, end, constellation, offset);
     return 0;
 }
